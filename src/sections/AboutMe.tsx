ea37c413712fc5ef795f885a6e812a94f3967e74
Jaya Jha <jayajha122006@gmail.com>
2025-08-11 13:06:41 +0530

    added inline style

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -2,7 +2,7 @@
 
 
 export const About=()=>{
-    return <div className="bg-[url('../bg4.png')] bg-cover bg-center flex flex-col items-center h-full w-full">
+    return <div className="bg-[url('/bg4.png')] bg-cover bg-center flex flex-col items-center h-full w-full">
                 <div className="flex  justify-center mb-10">
                     <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-400 to-purple-500 text-5xl pt-16 pb-3 font-bold border-b-4 border-purple-500  ">
                     About Me
@@ -11,7 +11,7 @@ export const About=()=>{
 
                 <div className="flex flex-row items-center h-full">
                     <div className="flex-1 px-20 ">     
-                        <img src="../photo.png" alt="Jaya" className=" w-120 h-120 aspect-square object-cover rounded-full border-4 border-purple-800 shadow-[0_0_30px_10px_rgba(192,132,252,0.5)]"></img>
+                        <img src="/photo.png" alt="Jaya" className=" w-120 h-120 aspect-square object-cover rounded-full border-4 border-purple-800 shadow-[0_0_30px_10px_rgba(192,132,252,0.5)]"></img>
                     </div>
                     <div className="">
                         <div className="flex-1 max-w-lg text-2xl text-violet-100">
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -20,7 +20,7 @@ export const Home=()=>{
         document.body.removeChild(link);
     };
 
-    return <div className="py-25 bg-[url('./bg3.png')] bg-cover bg-center justify-center items-center text-center flex flex-col  h-screen">
+    return <div style={{ backgroundImage: "url('/bg3.png')" }} className="  bg-cover bg-center py-25 justify-center items-center text-center flex flex-col  h-screen">
         <h1 className="text-7xl font-bold text-white">
             Hi,I'm {""}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-400 to-purple-500 font-bold text-7xl">Jaya Jha</span>
