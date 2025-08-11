8c47210ba812626ec21877baa768a7e4e0a2b840
Jaya Jha <jayajha122006@gmail.com>
2025-08-11 11:35:36 +0530

    changed bg of skills

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,35 +1,80 @@
-import '../index.css'
+import '../index.css';
 import { Skillcard } from "../components/skillcard";
-import html from '../assets/html.png'
-import css from '../assets/css.png'
-import js from '../assets/js.png'
-import nodejs from '../assets/nodejs.png'
-import express from '../assets/express.png'
-import ts from '../assets/ts.png'
-import react from '../assets/react.svg'
-import mongo from '../assets/mongo.png'
-import postgress from '../assets/postgress.png'
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import js from '../assets/js.png';
+import nodejs from '../assets/nodejs.png';
+import express from '../assets/express.png';
+import ts from '../assets/ts.png';
+import react from '../assets/react.svg';
+import mongo from '../assets/mongo.png';
+import postgress from '../assets/postgress.png';
 
-export const Skills=()=>{
-    return <div className="flex flex-col items-center min-h-screen w-full bg-svg overflow-y-auto">
-        <div className="flex  justify-center mb-10 w-full">
-                    <h2 className="text-white  text-5xl pt-20 pb-3 font-bold border-b-4 border-purple-500  ">
-                    Skills
-                    </h2>
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
+
+export const Skills = ({ showParticles }: { showParticles: boolean }) => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: Container | undefined) => {
+    console.log(container);
+  }, []);
+
+  return (
+    <section id="skills" className="relative w-full min-h-screen">
+      {/* Background image */}
+      <div className="absolute inset-0 -z-30 bg-svg " />
+
+      {/* Particles */}
+      {showParticles && (
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            "particles": {
+              "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+              "color": { "value": "#dae4ed" },
+              "shape": { "type": "circle" },
+              "opacity": { "value": 0.5 },
+              "size": { "value": 2, "random": true },
+              "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 },
+              "move": { "enable": true, "speed": 1, "out_mode": "bounce" }
+            },
+            "interactivity": {
+              "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true },
+              "modes": { "repulse": { "distance": 70, "duration": 0.4 }, "push": { "particles_nb": 4 } }
+            },
+            "retina_detect": true
+          }}
+          className="fixed top-0 left-0 w-full h-full -z-10"
+        />
+      )}
+
+      {/* Content */}
+      <div className="relative z-20 flex flex-col items-center w-full">
+        <div className="flex justify-center mb-10 w-full">
+          <h2 className="text-white text-5xl pt-20 pb-3 font-bold border-b-4 border-purple-500">
+            Skills
+          </h2>
         </div>
         <div className="flex flex-wrap justify-center w-full max-w-6xl">
-            <div className="w-1/4 p-4"><Skillcard image={html} name={"HTML"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={css} name={"CSS"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={js} name={"JavaScript"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={nodejs} name={"Node.js"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={express} name={"Express"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={react} name={"React.js"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={ts} name={"TypeScript"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={mongo} name={"MongoDB"} /></div>
-            <div className="w-1/4 p-4"><Skillcard image={postgress} name={"PostgreSQL"} /></div>
-            
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={html} name="HTML" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={css} name="CSS" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={js} name="JavaScript" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={nodejs} name="Node.js" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={express} name="Express" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={react} name="React.js" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={ts} name="TypeScript" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={mongo} name="MongoDB" /></div>
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"><Skillcard image={postgress} name="PostgreSQL" /></div>
         </div>
-                
-           
-    </div>
-}
+      </div>
+    </section>
+  );
+};
